63df49100cf5929abcea0739ed7ffdf8426f43e9
269774331 <51011573+269774331@users.noreply.github.com>
2020-01-18 21:35:33 +0800

    Update CangYuanTu.py

--- books/biquge/CangYuanTu.py
+++ books/biquge/CangYuanTu.py
@@ -11,4 +11,4 @@ def getBook():
 class CangYuanTu(xxbiqugebase):
     title = u'[小说]沧元图'
     description = u'我吃西红柿'
-    feeds = 'https://www.xsbiquge.com/86_86216/'
+    feeds = 'https://www.xxbiquge.com/86_86216/'
